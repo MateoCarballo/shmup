7ba9ca4a72c043818c6274fc473e9bf199c75b5b
MateoPortatil <mateocarballo2@gmail.com>
2025-05-06 19:16:51 +0200

    Intentando acabar el menu de pausa

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,14 +10,13 @@ public class GameManager : MonoBehaviour
     private Boolean enemyAlive = false;
     private Boolean isSpawning = false;
     private bool isPaused = false;
-    private String pauseSceneName = "PauseMenu";
-    private Canvas pauseCanvas;
+    public Canvas pauseCanvas;
     void Start()
     {
         //TODO la idea es que el objeto que tiene los puntos de spawn cree todos los enemigos y los active y desactive segun condiciones
         StartCoroutine(SpawnEnemy());
-        Canvas[] canvasIntances = (Canvas[])Resources.FindObjectsOfTypeAll(typeof(Canvas)); 
-        pauseCanvas = canvasIntances[0];
+       // Canvas[] canvasIntances = (Canvas[])Resources.FindObjectsOfTypeAll(typeof(Canvas)); 
+       // pauseCanvas = canvasIntances[0];
     }
 
     IEnumerator SpawnEnemy()
@@ -44,24 +43,40 @@ public class GameManager : MonoBehaviour
         {
             TogglePause(); // Metodo para gestionar el activar o desactivar la escena del menu de pausa
         }
-
+ 
     }
-
+    
     public void TogglePause()
     {
-        isPaused = !isPaused;
-
         if (isPaused)
         {
-            // Pausar juego y cargar escena de pausa
-            Time.timeScale = 0f;
-            SceneManager.LoadScene(pauseSceneName, LoadSceneMode.Additive);
+            ResumeGame();
         }
         else
         {
-            // Reanudar juego y descargar escena de pausa
-            Time.timeScale = 1f;
-            SceneManager.UnloadSceneAsync(pauseSceneName);
+            PauseGame();
         }
+        isPaused = !isPaused;
     }
+
+    public void PauseGame()
+    {
+        pauseCanvas.enabled = true; // Mostrar el menú de pausa
+        Time.timeScale = 0f; // Pausar el juego
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseCanvas.enabled = false; // Ocultar el menú de pausa
+        Time.timeScale = 1f; // Reanudar el juego
+        isPaused = false;
+    }
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1f; // Asegurar que el tiempo vuelve a la normalidad
+        SceneManager.LoadScene("MainMenu"); // Cargar el menú principal
+    }
+
 }
